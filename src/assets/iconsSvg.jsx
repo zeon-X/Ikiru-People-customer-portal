6185f98053b73239df877e2b689f4a180f21f902
Md. Shefat Zeon <mdshefatzeon@gmail.com>
2023-05-25 15:19:29 +0600

    first commit

--- src/assets/iconsSvg.jsx
+++ src/assets/iconsSvg.jsx
@@ -35,7 +35,6 @@ const homeSvg = (
     viewBox="0 0 20 17"
     fill="none"
     xmlns="http://www.w3.org/2000/svg"
-    stroke="currentColor"
   >
     <path d="M8 17V11H12V17H17V9H20L10 0L0 9H3V17H8Z" fill="#B6B4BA" />
   </svg>
